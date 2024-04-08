c246ba2bbbd3776d56ef9819c202724ac6ddab48
Alex <alexccv94@gmail.com>
2024-04-08 08:26:00 -0400

    fixed Betty style

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -5,6 +5,10 @@
  * @size: the size of array for printing func
  * @a: one int
  * @b: the other int
+ * kwiksort - quick sort via lamuto partition
+ * quick_sort - quickly sorts
+ * partition - "partition" array
+ *
  */
 void swaps(int *array, size_t size, int *a, int *b)
 {
@@ -16,14 +20,7 @@ void swaps(int *array, size_t size, int *a, int *b)
 		print_array((const int *)array, size);
 	}
 }
-/**
- * kwiksort - quick sort via lamuto partition
- * @array: the array
- * @size: size of array
- * @low: low [] of sort
- * @high: high [] of sort
- *
- */
+
 void kwiksort(int *array, size_t size, ssize_t low, ssize_t high)
 {
 	if (low < high)
@@ -35,12 +32,6 @@ void kwiksort(int *array, size_t size, ssize_t low, ssize_t high)
 	}
 }
 
-/**
- * quick_sort
- * @array: the array to sort
- * @size: size of array
- *
- */
 void quick_sort(int *array, size_t size)
 {
 	if (!array || !size)
@@ -48,14 +39,7 @@ void quick_sort(int *array, size_t size)
 
 	kwiksort(array, size, 0, size - 1);
 }
-/**
- * partition - "partition" array
- * @array: the array
- * @size: the size of array
- * @low: the bottom of the index
- * @high: the top of it
- * Return: size_t part, the partition value
- */
+
 size_t partition(int *array, size_t size, ssize_t low, ssize_t high)
 {
 
