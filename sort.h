f47ecdcc4b8e6e43ef66834da2c09284353fd5c6
Alex <alexccv94@gmail.com>
2024-04-08 08:11:26 -0400

    fixed some functions

--- sort.h
+++ sort.h
@@ -25,5 +25,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void swaps(int *array, size_t size, int *a, int *b);
+void kwiksort(int *array, size_t size, ssize_t low, ssize_t high);
+size_t partition(int *array, size_t size, ssize_t low, ssize_t high);
 
 #endif /* SORT_H */
